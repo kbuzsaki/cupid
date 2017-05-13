2a966aeedb1061efb2ec8edabe00310c60c4812c
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2017-05-12 17:23:08 -0700

    Added test for concurrent opens and reads

--- server/server_test.go
+++ server/server_test.go
@@ -1,6 +1,9 @@
 package server
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestServerImpl_KeepAlive(t *testing.T) {
 	s, err := New()
@@ -72,6 +75,72 @@ func TestServerImpl_OpenGetSet(t *testing.T) {
 
 func TestServerImpl_SetContentGeneration(t *testing.T) {
 	// TODO: test skipping writes for old generation
+}
+
+func TestServerImpl_ConcurrentOpen(t *testing.T) {
+	ne := func(m string, e error) {
+		if e != nil {
+			t.Error(m, e)
+		}
+	}
+
+	s, err := New()
+	if err != nil {
+		t.Fatal("Unable to start server:", err)
+	}
+
+	content := "some content"
+	mainDone1 := sync.WaitGroup{}
+	mainDone2 := sync.WaitGroup{}
+	childrenDone := sync.WaitGroup{}
+	childCount := 100
+
+	mainDone1.Add(1)
+	for i := 0; i < childCount; i++ {
+		go func() {
+			// wait until all children are created to do concurrent open
+			mainDone1.Wait()
+
+			nd, err := s.Open("/foo/baz", false)
+			ne("Error opening file in child:", err)
+
+			// signal that this child is done and wait until main is done
+			childrenDone.Done()
+			mainDone2.Wait()
+
+			// verify that the node descriptor from earlier reads the correct data
+			cas, err := s.GetContentAndStat(nd)
+			ne("Error getting content in child:", err)
+
+			if cas.Content != content {
+				t.Errorf("Read incorrect content in child: %#v\n", cas.Content)
+			}
+
+			// finally tell main that this child is done
+			childrenDone.Done()
+		}()
+	}
+
+	// once all of the children are created, signal them to open and wait until they all do
+	childrenDone.Add(childCount)
+	mainDone2.Add(1)
+	mainDone1.Done()
+	childrenDone.Wait()
+
+	// set the content so that all of the children can read it
+	nd, err := s.Open("/foo/baz", false)
+	ne("Error opening file in main:", err)
+
+	ok, err := s.SetContent(nd, content, 10)
+	ne("Error setting content from main:", err)
+	if !ok {
+		t.Error("Failed to set content from main")
+	}
+
+	// then wait for all of the children to finish reading
+	childrenDone.Add(childCount)
+	mainDone2.Done()
+	childrenDone.Wait()
 }
 
 func TestServerImpl_TryAcquire(t *testing.T) {
