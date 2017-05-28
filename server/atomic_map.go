663719e58aef7ff6eb586ac83fab69b499558e9d
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2017-05-28 15:19:39 -0700

    Added Keys() method to atomic map

--- server/atomic_map.go
+++ server/atomic_map.go
@@ -5,6 +5,7 @@ import "sync"
 type AtomicMap interface {
 	Get(k uint64) interface{}
 	Put(k uint64, v interface{})
+	Keys() []uint64
 }
 
 type atomicMapImpl struct {
@@ -28,4 +29,16 @@ func (am *atomicMapImpl) Put(k uint64, v interface{}) {
 	am.lock.Lock()
 	defer am.lock.Unlock()
 	am.data[k] = v
+}
+
+func (am *atomicMapImpl) Keys() []uint64 {
+	am.lock.RLock()
+	defer am.lock.RUnlock()
+	var keys []uint64
+
+	for key := range am.data {
+		keys = append(keys, key)
+	}
+
+	return keys
 }
