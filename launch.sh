2e7965391712590fee49eb7f071a00c16c67c8ac
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2017-06-15 14:57:07 -0700

    Add none replicas to launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+NO_CLUSTER='none'
 ONE_CLUSTER='http://127.0.0.1:12379'
 THREE_CLUSTER='http://127.0.0.1:12379,http://127.0.0.1:22379,http://127.0.0.1:32379'
 FIVE_CLUSTER='http://127.0.0.1:12379,http://127.0.0.1:22379,http://127.0.0.1:32379,http://127.0.0.1:42379,http://127.0.0.1:52379'
@@ -17,21 +18,25 @@ function killservers() {
 
 trap "killservers" SIGINT
 
-CLUSTER_ARG=""
-if [ "$1" = "1" ]; then
-    CLUSTER_ARG=$ONE_CLUSTER
+replicas=$1
+cluster_arg=""
+if [ "$1" = "0" ]; then
+    replicas=1
+    cluster_arg=$NO_CLUSTER
+elif [ "$1" = "1" ]; then
+    cluster_arg=$ONE_CLUSTER
 elif [ "$1" = "3" ]; then
-    CLUSTER_ARG=$THREE_CLUSTER
+    cluster_arg=$THREE_CLUSTER
 elif [ "$1" = "5" ]; then
-    CLUSTER_ARG=$FIVE_CLUSTER
+    cluster_arg=$FIVE_CLUSTER
 else
     echo "invalid arg number of replicas: '$1'"
     exit 1
 fi
 
-for id in $(seq 1 $1); do
+for id in $(seq 1 $replicas); do
     echo "launching $id"
-    cupid-server --id $id --cluster $CLUSTER_ARG --port "$id"2380 &
+    cupid-server --id $id --cluster $cluster_arg --port "$id"2380 &
     pids="$pids $!"
 done
 
